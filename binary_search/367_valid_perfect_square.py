3881d6bf98d739abee28387efed4c71c34459ed7
blockvished <jcmvishael@gmail.com>
2025-08-22 16:04:18 +0000

    solve using binary search

--- binary_search/367_valid_perfect_square.py
+++ binary_search/367_valid_perfect_square.py
@@ -11,3 +11,22 @@ class Solution:
 ```
 for i in range(num+1):
 ```
+
+# wasnt sure if it could be solved with bs but figured after looking at editorial title
+# so solving without looking at solution of code
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        s = 0
+        e = num
+
+        while s <= e:
+            m = (s+e)//2
+
+            if m*m == num:
+                return True
+            elif m*m > num:
+                e = m-1
+            else:
+                s = m+1
+        return False
