7143d0c162901dfcec89f1f806211bcf2e039124
blockvished <jcmvishael@gmail.com>
2025-08-22 15:34:52 +0000

    solve insert position

---
+++ binary_search/35_search_insert_position.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        s = 0
+        e = len(nums) - 1
+
+        while s <= e:
+            m = (s+e)//2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                s = s+1
+            else:
+                e = m-1
+        return e+1
+
+# took tinkering for multiple tries but ez as before
